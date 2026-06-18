e2568f81d9775aeae7c76169c1f42fad3d170ce0
gustavod25 <gustavodf25@gmail.com>
2026-06-18 19:05:02 -0300

    estructura inicial

---
+++ src/main/java/ar/edu/unlar/SHIFTParcial2/ShiftParcial2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.SHIFTParcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftParcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftParcial2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlar/SHIFTParcial2/ShiftParcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.SHIFTParcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftParcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
